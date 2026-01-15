3606639840798dcde69c7a003fe90351850d4592
samaydnaik <samronsrdjnaik@gmail.com>
2026-01-15 23:38:36 +0530

    Initial Setup

---
+++ src/main/java/com/aiFinanceTracker/track/AiFinanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.aiFinanceTracker.track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiFinanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiFinanceTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aiFinanceTracker/track/AiFinanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiFinanceTracker.track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiFinanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
